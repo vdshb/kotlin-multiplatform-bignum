ddf254abb3451c384953274d0eebabb270f977a4
vdshb <github.com/vdshb>
2025-07-29 01:10:45 +0500

    add negative tests

--- bignum/src/jvmTest/kotlin/com/ionspin/kotlin/bignum/decimal/BigDecimalJvmTest.kt
+++ bignum/src/jvmTest/kotlin/com/ionspin/kotlin/bignum/decimal/BigDecimalJvmTest.kt
@@ -121,6 +121,44 @@ class BigDecimalJvmTest {
             val javaBigDecimal = java.math.BigDecimal.ONE
             javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
         }
+
+        assertTrue {
+            val bigDecimal = BigDecimal.fromLongWithExponent(-123L, 5)
+            val javaBigDecimal = java.math.BigDecimal.valueOf(-123, -3)
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
+        assertTrue {
+            val bigDecimal = BigDecimal.fromLongWithExponent(-123L, 4)
+            val javaBigDecimal = java.math.BigDecimal.valueOf(-123, -2)
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
+        assertTrue {
+            val bigDecimal = BigDecimal.fromLongWithExponent(-71, -2)
+            val javaBigDecimal = java.math.BigDecimal.valueOf(-71, 3)
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
+        assertTrue {
+            val bigDecimal = BigDecimal.fromLongWithExponent(-125L, -7)
+            val javaBigDecimal = java.math.BigDecimal.valueOf(-125, 9)
+            val compar = javaBigDecimal.toKotlinBigDecimal()
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
+        assertTrue {
+            val bigDecimal = BigDecimal.fromLongWithExponent(-71, 15)
+            val javaBigDecimal = java.math.BigDecimal.valueOf(-71, -14)
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
+        assertTrue {
+            val bigDecimal = - BigDecimal.ONE
+            val javaBigDecimal = - java.math.BigDecimal.ONE
+            javaBigDecimal.toKotlinBigDecimal().compareTo(bigDecimal) == 0
+        }
+
     }
 
     @Test
